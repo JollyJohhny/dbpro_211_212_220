b62753eb9235b50cd9eaef2641413dfbf474ff12
Adnan Aashiq <adnanaashiq457@gmail.com>
2019-04-25 22:07:26 +0500

    By Adnan

---
+++ SmartSchoolWebPortal/SmartSchoolWebPortal/Models/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartSchoolWebPortal.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string RegisterationNumber { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
