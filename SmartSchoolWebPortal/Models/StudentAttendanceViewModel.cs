c1cd8018d25f891de0850775958492968496549b
Adnan Aashiq <adnanaashiq457@gmail.com>
2019-04-29 08:26:21 +0500

    Final Touch

---
+++ SmartSchoolWebPortal/Models/StudentAttendanceViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartSchoolWebPortal.Models
+{
+    public class StudentAttendanceViewModel
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Id")]
+        public int ClassAttendaceId { get; set; }
+
+        [Display(Name = "Id")]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+    }
+}
